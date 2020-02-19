7b7811146ce74e16d25682e8c6ac0fc988089c67
sehyeon <sehyeonoh97@gmail.com>
2020-02-19 13:51:25 -0800

    making changes to getdisplaymedia

--- video.js
+++ video.js
@@ -21,20 +21,20 @@ stopElem.addEventListener("click", function() {
 
 
 async function startCapture() {
-    // logElem.innerHTML = "";
-    // try {
-    //     videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
-    //     dumpOptionsInfo();
-    // } catch(err) {
-    //     console.error("Error: " + err);
-    // }
-    if (navigator.getDisplayMedia) {
-        return navigator.getDisplayMedia({video: true});
-    } else if (navigator.mediaDevices.getDisplayMedia) {
-        return navigator.mediaDevices.getDisplayMedia({video: true});
-    } else {
-        return navigator.mediaDevices.getUserMedia({video: true});
+    logElem.innerHTML = "";
+    try {
+        videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+        dumpOptionsInfo();
+    } catch(err) {
+        console.error("Error: " + err);
     }
+    // if (navigator.getDisplayMedia) {
+    //     return navigator.getDisplayMedia({video: true});
+    // } else if (navigator.mediaDevices.getDisplayMedia) {
+    //     return navigator.mediaDevices.getDisplayMedia({video: true});
+    // } else {
+    //     return navigator.mediaDevices.getUserMedia({video: true});
+    // }
 }
 
 function stopCapture(evt) {
