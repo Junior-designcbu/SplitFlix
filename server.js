c31261b4ddcefd5f3dad91d59b0405821205cf42
sehyeon <sehyeonoh97@gmail.com>
2020-02-19 13:05:10 -0800

    think bug is found

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-// import socketIO from "socket.io";
-// import createServer from "http";
 const httpServer = require('https');
 const io = require('socket.io')(httpServer);
 
 const express = require('express');
 const app = express();
 const path = require('path');
-// const httpServer = createServer(app);
-// const io = socketIO(httpServer);
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
