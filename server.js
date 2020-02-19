d60875a535403790f080a3182fa92b0d28a4fa81
sehyeon <sehyeonoh97@gmail.com>
2020-02-19 12:47:25 -0800

    where is the bug

--- server.js
+++ server.js
@@ -1,22 +1,46 @@
 import socketIO from "socket.io";
 // import createServer from "http";
-const httpServer = require('https');
-const io = require('socket.io')(httpServer);
+// const httpServer = require('https');
+// const io = require('socket.io')(httpServer);
+//
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// // const httpServer = createServer(app);
+// // const io = socketIO(httpServer);
+//
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, 'index.html'));
+// });
+//
+// io.on("connection", socket => {
+//     console.log("Socket connected.");
+// });
+//
+// app.listen(process.env.PORT || 4000, function(){
+//     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
 
-const express = require('express');
-const app = express();
-const path = require('path');
-// const httpServer = createServer(app);
-// const io = socketIO(httpServer);
+var app = require('express')(),
+    http = require('http').createServer(app),
+    io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/client/index.html');
 });
 
-io.on("connection", socket => {
-    console.log("Socket connected.");
+io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
 });
 
-app.listen(process.env.PORT || 4000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+http.listen(process.env.PORT || 3000, function () {
+    console.log('listening on *:3000');
 });
