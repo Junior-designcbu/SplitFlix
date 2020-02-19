080318ad2325d0514cf25b59d626d9bf97d15a92
sehyeon <sehyeonoh97@gmail.com>
2020-02-19 12:38:06 -0800

    connecting socket io

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
-// import socketIO from "socket.io";
+import socketIO from "socket.io";
 // import createServer from "http";
+const httpServer = require('http');
+const io = require('socket.io')(httpServer);
 
 const express = require('express');
 const app = express();
@@ -11,10 +13,10 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+io.on("connection", socket => {
+    console.log("Socket connected.");
+});
+
 app.listen(process.env.PORT || 4000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-
-// io.on("connection", socket => {
-//     console.log("Socket connected.");
-// });
