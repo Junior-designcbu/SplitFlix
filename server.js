323a61885b6464749ab29a837f97b8f4fe66bec6
sehyeon <sehyeonoh97@gmail.com>
2020-02-19 21:23:49 -0800

    httpserver listen

--- server.js
+++ server.js
@@ -17,3 +17,7 @@ io.on("connection", socket => {
 app.listen(process.env.PORT || 4000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+httpServer.listen(process.env.PORT || 4000, function() {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
