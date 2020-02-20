aca0a0843645f3e5af06cd443427ffae8c835fde
sehyeon <sehyeonoh97@gmail.com>
2020-02-19 16:00:06 -0800

    hmm

--- video.js
+++ video.js
@@ -28,13 +28,6 @@ async function startCapture() {
     } catch(err) {
         console.error("Error: " + err);
     }
-    // if (navigator.getDisplayMedia) {
-    //     return navigator.getDisplayMedia({video: true});
-    // } else if (navigator.mediaDevices.getDisplayMedia) {
-    //     return navigator.mediaDevices.getDisplayMedia({video: true});
-    // } else {
-    //     return navigator.mediaDevices.getUserMedia({video: true});
-    // }
 }
 
 function stopCapture(evt) {
