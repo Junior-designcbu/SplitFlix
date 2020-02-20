e91b1ef0b3412517290370b74762121e2ec9d035
sehyeon <sehyeonoh97@gmail.com>
2020-02-20 14:01:25 -0800

    deleted __dirname

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const path = require('path');
 const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer);
 
-app.listen(process.env.PORT || 4000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
 // our express middleware, serving all files under directory of root
 app.use(express.static(path.join( "root")));
 
 //upon get request to server, respond with index.html
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join('root', 'index.html'));
 });
 
 io.on("connection", socket => {
     console.log("Socket connected.");
+});
+
+app.listen(process.env.PORT || 4000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
