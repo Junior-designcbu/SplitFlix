77abdafd4c135d0a6d7c22b620cf2cda1c78b149
sehyeon <sehyeonoh97@gmail.com>
2020-02-19 22:16:38 -0800

    change of order

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const httpServer = require('http').createServer(app);
 const io = require('socket.io')(httpServer);
 
+app.use(express.static(path.join(__dirname, "root")));
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-app.use(express.static(path.join(__dirname, "root")));
 
 io.on("connection", socket => {
     console.log("Socket connected.");
