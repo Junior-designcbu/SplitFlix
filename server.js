13ba938fc7ec451ed4eda399c0952143b5e43ff5
sehyeon <sehyeonoh97@gmail.com>
2020-02-20 16:51:03 -0800

    changing respond file

--- server.js
+++ server.js
@@ -9,8 +9,7 @@ app.use(express.static(path.join( "root")));
 
 //upon get request to server, respond with index.html
 app.get('/', function(req, res) {
-    res.sendFile('./index.html');
-
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 io.on("connection", socket => {
