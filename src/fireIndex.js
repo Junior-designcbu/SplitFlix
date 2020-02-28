dde31e8e8ee0c9dde50f8939bacbde7aa25aa7f1
RigobertoVincent <Rigobertovicente.Gonzalez@calbaptist.edu>
2020-02-28 10:33:02 -0800

    updated files

--- src/fireIndex.js
+++ src/fireIndex.js
@@ -1,32 +1,32 @@
-var db = firebase.firestore();
-
-function storeData() {
-
-    var nameText = document.getElementById("name_field").value;
-    var emailText = document.getElementById("email_field").value;
-    var PasswordText = document.getElementById("password_field").value;
-    // Add a new document in collection "cities"
-    db.collection("newUsers").doc().set({
-        name: nameText,
-        email: emailText,
-        password: PasswordText
-
-    }).then(function() {
-        console.log("Document successfully written!");
-    })
-}
-
-function storeLogin() {
-    var loginEmailText = document.getElementById("login_email").value;
-    var loginPasswordText = document.getElementById("login_password").value;
-    // Add a new document in collection "cities"
-    db.collection("currentUsers").doc().set({
-        email: loginEmailText,
-        password: loginPasswordText
-
-    }).then(function() {
-        console.log("Document successfully written!");
-    })
+// var db = firebase.firestore();
+//
+// function storeData() {
+//
+//     var nameText = document.getElementById("name_field").value;
+//     var emailText = document.getElementById("email_field").value;
+//     var PasswordText = document.getElementById("password_field").value;
+//     // Add a new document in collection "cities"
+//     db.collection("newUsers").doc().set({
+//         name: nameText,
+//         email: emailText,
+//         password: PasswordText
+//
+//     }).then(function() {
+//         console.log("Document successfully written!");
+//     })
+// }
+//
+// function storeLogin() {
+//     var loginEmailText = document.getElementById("login_email").value;
+//     var loginPasswordText = document.getElementById("login_password").value;
+//     // Add a new document in collection "cities"
+//     db.collection("currentUsers").doc().set({
+//         email: loginEmailText,
+//         password: loginPasswordText
+//
+//     }).then(function() {
+//         console.log("Document successfully written!");
+//     })
 }
 
 // function writeUserData(userId, name, email, password) {
