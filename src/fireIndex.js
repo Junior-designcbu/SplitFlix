217d49ac169063942b675a635ed3a72a2533c7ad
aaron yanes <aaronyanes@aarons-MacBook-Pro.local>
2020-02-28 11:47:52 -0800

    update auth

--- src/fireIndex.js
+++ src/fireIndex.js
@@ -1,42 +1,32 @@
-// var db = firebase.firestore();
-//
-// function storeData() {
-//
-//     var nameText = document.getElementById("name_field").value;
-//     var emailText = document.getElementById("email_field").value;
-//     var PasswordText = document.getElementById("password_field").value;
-//     // Add a new document in collection "cities"
-//     db.collection("newUsers").doc().set({
-//         name: nameText,
-//         email: emailText,
-//         password: PasswordText
-//
-//     }).then(function() {
-//         console.log("Document successfully written!");
-//     })
-// }
-//
-// function storeLogin() {
-//     var loginEmailText = document.getElementById("login_email").value;
-//     var loginPasswordText = document.getElementById("login_password").value;
-//     // Add a new document in collection "cities"
-//     db.collection("currentUsers").doc().set({
-//         email: loginEmailText,
-//         password: loginPasswordText
-//
-//     }).then(function() {
-//         console.log("Document successfully written!");
-//     })
-}
+// Your web app's Firebase configuration
+(function() {
+    var firebaseConfig = {
+        apiKey: "AIzaSyD-XesmW_0wA7e7DtNYhUECmhgJssPYf3w",
+        authDomain: "splitflix-17650.firebaseapp.com",
+        databaseURL: "https://splitflix-17650.firebaseio.com",
+        projectId: "splitflix-17650",
+        storageBucket: "splitflix-17650.appspot.com",
+        messagingSenderId: "761563690321",
+        appId: "1:761563690321:web:61c178c393597089534f70",
+        measurementId: "G-63BMXK45X9"
+    };
 
-// function writeUserData(userId, name, email, password) {
-//     var nameText = document.getElementById("name_field").value;
-//     var emailText = document.getElementById("email_field").value;
-//     var PasswordText = document.getElementById("password_field").value;
-//     firebase.database().ref('users/' + userId).set({
-//         username: nameText,
-//         email: emailText,
-//         password : PasswordText
-//     });
-// }
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+
+    // GET DOM ELEMENTS
+    const emailTxt = document.getElementById("email_field");
+    const passwordTxt = document.getElementById("password_field");
+    const btnLogin = document.getElementById("sign_in_btn");
+
+    // add login event
+    btnLogin.addEventListener('click', e => {
+        // get email and password
+        const email = emailTxt.value;
+        const password = passwordTxt.value;
+        const auth = firebase.auth();
+    })
+
+}());
+
 
